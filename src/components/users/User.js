b209e275d3c86292c08a68424d8a2587431f599e
osakoh <macmive@gmail.com>
2021-04-30 13:39:22 +0100

    converted the UserJS class component to a functional component & implemented useEffect hook

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,93 +1,104 @@
-import React, { Component, Fragment } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import Spinner from '../layout/Spinner';  // Spinner import
 import Repos from '../Repos/Repos';
 import PropTypes from 'prop-types' // PropType import: impt
 import { Link } from 'react-router-dom';
 
 
+// destructuring from props
+const User = ({ user, loading, repos, getSingleUser, getUserRepos, match }) => {
 
-class User extends Component {
+    // useEffect hook allows components to have access to the lifecycle events of a component/mimics the lifecycle component(componentDidMount)
+    // this is first run by React when the component is first rendered, and on every subsequent render/update 
+    useEffect(() => {
+        // params.login: gets the username from the url; 'login' is the parameter used when defining the path in App JS
+        getSingleUser(match.params.login);
+        getUserRepos(match.params.login);
+        // eslint-disable-next-line
+    }, []);
+    /**
+    eslint-disable-next-line: disables the dependency warnings for getSingleUser, getUserRepos & match.params.login
+    useEffect() function is run on every subsequent re-render/update of the component, we can tell React to
+    skip it, for performance purposes, by adding a second parameter which is an array that contains a list 
+    of state variables to watch for. React will only re-run the side effect if oneof the items in this array changes.
+    Also, you can tell React to only execute the side effect once (at mount time), by passing an empty array:
+    */
 
-    componentDidMount() {
-        // props.match.params.login: gets the username from the url; 'login' is the parameter used when defining the path in App JS
-        this.props.getSingleUser(this.props.match.params.login);
-        this.props.getUserRepos(this.props.match.params.login);
-    }
+    // destructuring from user
+    const { name, company, avatar_url, location, bio, blog, login, html_url,
+        followers, following, public_repos, public_gists, hireable } = user;
 
-    // Proptypes
-    static propTypes = {
-        loading: PropTypes.bool,
-        user: PropTypes.object.isRequired,
-        getSingleUser: PropTypes.func.isRequired,
-        getUserRepos: PropTypes.func.isRequired,
-        repos: PropTypes.array.isRequired,
-    }
-
-    render() {
-        const { repos, loading, user: { name, company, avatar_url, location, bio, blog, login, html_url,
-            followers, following, public_repos, public_gists, hireable } } = this.props;
-
-        if (loading) return <Spinner />;
-        return (
-            <Fragment>
-                <Link to='/' className='btn btn-light'>Back to Homepage</Link>
+    if (loading) return <Spinner />;
+    return (
+        <Fragment>
+            <Link to='/' className='btn btn-light'>Back to Homepage</Link>
                     Hireable: {'  '}
-                {hireable ? <i className="fas fa-check text-success"></i> :
-                    <i className="fas fa-times-circle text-danger"></i>}
+            {hireable ? <i className="fas fa-check text-success"></i> :
+                <i className="fas fa-times-circle text-danger"></i>}
 
-                <div className="card grid-2" style={{ marginTop: "40px" }}>
+            <div className="card grid-2" style={{ marginTop: "40px" }}>
 
-                    <div className="all-center">
-                        <img src={avatar_url} alt="Profile Picture" className="round-img" style={{ width: " 150px" }} />
-                        <p>Name: {name}</p>
-                        <p>Location: {location}</p>
-                    </div>
-
-                    <div>
-                        {bio &&
-                            (<Fragment>
-                                <h3>Bio</h3>
-                                <p>{bio}</p>
-                            </Fragment>)}
-                        <a href={html_url} className="btn btn-dark my-1" target="__blank">View GitHub profile</a>
-
-                        <li>
-                            {login && (
-                                <Fragment>
-                                    <strong>Username: </strong>{login}
-                                </Fragment>
-                            )}
-                        </li>
-                        <li>
-                            {company && (
-                                <Fragment>
-                                    <strong>Company: </strong>{company}
-                                </Fragment>
-                            )}
-                        </li>
-                        <li>
-                            {blog && (
-                                <Fragment>
-                                    <strong>Website: </strong><a href={blog} target="__blank">{blog}</a>
-                                </Fragment>
-                            )}
-                        </li>
-                    </div>
+                <div className="all-center">
+                    <img src={avatar_url} alt="Profile" className="round-img" style={{ width: " 150px" }} />
+                    <p>Name: {name}</p>
+                    <p>Location: {location}</p>
                 </div>
 
-                <div className="card text-center" style={{ marginTop: "15px" }}>
-                    <div className="badge badge-primary">Followers: {followers}</div>
-                    <div className="badge badge-success">Following: {following}</div>
-                    <div className="badge badge-danger">Public Repos: {public_repos}</div>
-                    <div className="badge badge-black">Public Gists: {public_gists}</div>
+                <div>
+                    {bio &&
+                        (<Fragment>
+                            <h3>Bio</h3>
+                            <p>{bio}</p>
+                        </Fragment>)}
+                    <a href={html_url} className="btn btn-dark my-1" target="__blank">View GitHub profile</a>
 
+                    <li>
+                        {login && (
+                            <Fragment>
+                                <strong>Username: </strong>{login}
+                            </Fragment>
+                        )}
+                    </li>
+                    <li>
+                        {company && (
+                            <Fragment>
+                                <strong>Company: </strong>{company}
+                            </Fragment>
+                        )}
+                    </li>
+                    <li>
+                        {blog && (
+                            <Fragment>
+                                <strong>Website: </strong><a href={blog} target="__blank">{blog}</a>
+                            </Fragment>
+                        )}
+                    </li>
                 </div>
+            </div>
 
-                <Repos repos={repos} />
+            <div className="card text-center" style={{ marginTop: "15px" }}>
+                <div className="badge badge-primary">Followers: {followers}</div>
+                <div className="badge badge-success">Following: {following}</div>
+                <div className="badge badge-danger">Public Repos: {public_repos}</div>
+                <div className="badge badge-black">Public Gists: {public_gists}</div>
 
-            </Fragment>)
+            </div>
 
-    }
+            <Repos repos={repos} />
+
+        </Fragment>
+    );
 }
 
-export default User
+
+// Proptypes
+User.propTypes = {
+    loading: PropTypes.bool,
+    user: PropTypes.object.isRequired,
+    getSingleUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired,
+    repos: PropTypes.array.isRequired,
+}
+
+
+export default User;
