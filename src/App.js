7d92e9175e4d4cf29e0147ad51018e6f782449b7
osakoh <macmive@gmail.com>
2021-05-03 13:34:32 +0100

    imported AlertState and wrapped application

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Search from "./components/users/Search"; // Search import
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
 import GithubState from "./context/github/GithubState";
+import AlertState from "./context/alert/AlertState";
 
 import "./App.css";
 
@@ -25,38 +26,40 @@ const App = () => {
 
   return (
     <GithubState>
-      <Router>
-        <div className="App">
-          <Navbar />
-          {/* passing the users & loading as props into the Users component */}
-          {/* searchUsers: must be the same name as that in Search js */}
-          <div className="container">
-            <Alert alert={alert} />
-            <Switch>
-              {/* route for home page which contains multiple components */}
-              <Route
-                exact
-                path="/"
-                render={(props) => (
-                  <Fragment>
-                    <Search showAlert={showAlert} />
-                    <Users />
-                  </Fragment>
-                )}
-              />
-              {/* route for home page which contains multiple components */}
+      <AlertState>
+        <Router>
+          <div className="App">
+            <Navbar />
+            {/* passing the users & loading as props into the Users component */}
+            {/* searchUsers: must be the same name as that in Search js */}
+            <div className="container">
+              <Alert alert={alert} />
+              <Switch>
+                {/* route for home page which contains multiple components */}
+                <Route
+                  exact
+                  path="/"
+                  render={(props) => (
+                    <Fragment>
+                      <Search showAlert={showAlert} />
+                      <Users />
+                    </Fragment>
+                  )}
+                />
+                {/* route for home page which contains multiple components */}
 
-              {/* route for about, contains a single component */}
-              <Route exact path="/about" component={About} />
-              {/* route for about, contains a single component */}
+                {/* route for about, contains a single component */}
+                <Route exact path="/about" component={About} />
+                {/* route for about, contains a single component */}
 
-              {/* route to display a single user, contains a single component with props; 'login' is the username passed as part of the url*/}
-              <Route exact path="/user/:login" component={User} />
-              {/* route to display a single user, contains a single component with props; 'login' is the username passed as part of the url*/}
-            </Switch>
+                {/* route to display a single user, contains a single component with props; 'login' is the username passed as part of the url*/}
+                <Route exact path="/user/:login" component={User} />
+                {/* route to display a single user, contains a single component with props; 'login' is the username passed as part of the url*/}
+              </Switch>
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
+      </AlertState>
     </GithubState>
   );
 };
