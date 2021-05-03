af86c5ca0b45abe82c02bf843411145c04ed184e
osakoh <macmive@gmail.com>
2021-05-03 14:12:15 +0100

    created NotFound component

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const NotFound = () => (
+  <div>
+    <h2>Page not found!</h2>
+    <p className="lead">This page does not exist.</p>
+  </div>
+);
+
+export default NotFound;
